83ce3d860efdddacedde0e327c021395ca5a0b35
Klara Mitrovic <klaramitrovic6@gmail.com>
2022-10-07 15:37:00 +0200

    Deleted comment

--- functions/src/helpers/helpers.ts
+++ functions/src/helpers/helpers.ts
@@ -1,6 +1,5 @@
 import { CreateRequest, Comment } from "../models/models";
 
 export const createCommentRequest = (comment: CreateRequest): Comment => {
- //logika koja poziva funkciju koja menja body comment-a
   return { ...comment, date: Date.now().toString()};
 };
